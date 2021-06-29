e752b170916b64625bd0c772f8df5be670f0b308
Jayanth <jayanth.koralla@gmail.com>
2021-06-29 14:00:33 +0530

    Add stock lookup form and regular stock lookup functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,11 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: 'Tpk_38459c5eabaa451597cf7ccdcec64097',
                               endpoint: 'https://sandbox.iexapis.com/v1' )
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol,name: client.company(ticker_symbol).company_name ,last_price: client.price(ticker_symbol))
+      rescue => exception
+        return nil
+    end
   end
+  
 end
