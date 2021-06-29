85e6efffe05f01383bf61b7cf8a15847ede74e69
Jayanth <jayanth.koralla@gmail.com>
2021-06-29 13:27:17 +0530

    Build my portfolio view and stock lookup form

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'Tpk_38459c5eabaa451597cf7ccdcec64097',
+                              endpoint: 'https://sandbox.iexapis.com/v1' )
+    client.price(ticker_symbol)
+  end
 end
