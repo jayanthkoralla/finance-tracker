d7702b64a66a469a033891b981973f06fcad7e20
Jayanth <jayanth.koralla@gmail.com>
2021-06-29 18:46:14 +0530

    Added third model and done with working form console

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users ,through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: 'Tpk_38459c5eabaa451597cf7ccdcec64097',
@@ -9,5 +13,5 @@ class Stock < ApplicationRecord
         return nil
     end
   end
-  
+
 end
